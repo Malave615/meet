b781ff52d3786993958c6e81895b6d1a3e45c1d4
Malave615 <malavetracy@gmail.com>
2024-12-08 14:20:52 -0500

    Installing & Setup for Atatus

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import * as atatus from 'atatus-spa';
+atatus.config('7885bfe40d7c4d90b0cae01389a2b6a6').install();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -21,3 +24,5 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+atatus.notify(new Error('Test Atatus Setup'));
