44fc095455cd19f02f52a76503fd0a22c5637390
Malave615 <malavetracy@gmail.com>
2024-12-18 13:42:15 -0500

    Set WarningAlert messages for when user is offline

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
-import { InfoAlert, ErrorAlert } from './components/Alert';
+import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
 
 import './App.css';
 
@@ -16,8 +16,16 @@ const App = () => {
   const [currentNOE, setCurrentNOE] = useState(32);
   const [infoAlert, setInfoAlert] = useState("");
   const [errorAlert, setErrorAlert] = useState("");
+  const [warningAlert, setWarningAlert] = useState("");
 
   useEffect(() => {
+    if ( navigator.onLine) {
+      // User is online, clear any warning messages
+      setWarningAlert("");
+    } else {
+      // User is offline, set a warning message
+      setWarningAlert("You are currently offline. Some features may not be available.");
+    }
     fetchData();
   }, [currentCity, currentNOE]);
 
@@ -35,6 +43,7 @@ const App = () => {
       <div className="alerts-container">
         {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
         {errorAlert && <ErrorAlert text={errorAlert} />}
+        {warningAlert && <WarningAlert text={warningAlert} />}
       </div>
       <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} setInfoAlert={setInfoAlert} />
       <NumberOfEvents currentNOE={currentNOE} setCurrentNOE={setCurrentNOE} errorAlert={errorAlert} setErrorAlert={setErrorAlert} />
