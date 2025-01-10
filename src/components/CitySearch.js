7c6de8e470e55a8a3cd576b358b7d675e87efc1c
Malave615 <malavetracy@gmail.com>
2025-01-10 09:15:24 -0500

    Changing where the city suggestions box appears.

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,12 +1,13 @@
 // src/components/CitySearch.js
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import './CitySearch.scss';
 
 const CitySearch = ({ allLocations, setCurrentCity, setInfoAlert }) => {
   const [showSuggestions, setShowSuggestions] = useState(false);
   const [query, setQuery] = useState("");
   const [suggestions, setSuggestions] = useState([]);
+  const inputRef = useRef(null);
 
   useEffect(() => {
     setSuggestions(allLocations);
@@ -42,6 +43,7 @@ const CitySearch = ({ allLocations, setCurrentCity, setInfoAlert }) => {
     <div data-testid="city-search" className="city-search-container">
       <h1>Meet App</h1>
       <input
+        ref={inputRef}
         type="text"
         className="city"
         placeholder="Search for a city"
@@ -49,19 +51,25 @@ const CitySearch = ({ allLocations, setCurrentCity, setInfoAlert }) => {
         onFocus={() => setShowSuggestions(true)}
         onChange={handleInputChanged}
       />
-      {showSuggestions ?
-        <ul className="suggestions">
-          {suggestions.map((suggestion) => {
-            return <li onClick={handleItemClicked} key={suggestion}>{suggestion}</li>
-          })}
+      {showSuggestions && suggestions.length > 0 && (
+        <ul 
+          className="suggestions"
+          style={{ 
+            top: inputRef.current ? `${inputRef.current.getBoundingClientRect().bottom}px` : "45px",
+            left: inputRef.current ? `${inputRef.current.getBoundingClientRect().left}px` : "0px", // Dynamically set left based on input position
+            width: inputRef.current ? `${inputRef.current.offsetWidth}px` : "100%", // Dynamically set width to match input width
+        }}
+        >
+          {suggestions.map((suggestion) => (
+            <li onClick={handleItemClicked} key={suggestion}>{suggestion}</li>
+          ))}
           <li key='See all cities' onClick={handleItemClicked}>
             <b>See all cities</b>
           </li>
         </ul>
-        : null
-      }
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default CitySearch;
